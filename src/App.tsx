e97de4652f9ef1314ab9538be2a21621eb1a5c4a
Fabricio Bedin <fabricioobedin@gmail.com>
2020-11-09 14:48:41 -0300

    Changing App file to use new providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
 import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import AppProvider from './hooks';
+
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <>
-    <StatusBar barStyle="dark-content" backgroundColor="#312E37" />
-    <View style={{ flex: 1, backgroundColor: '#312E37' }}>
-      {/* <Routes /> */}
-    </View>
-  </>
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <AppProvider>
+      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+        <Routes />
+      </View>
+    </AppProvider>
+  </NavigationContainer>
 );
 
 export default App;
